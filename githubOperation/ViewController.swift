3ff93972a7d76a74d8eaa3a6c6aeb933b2262d01
faizanpro77 <faizyshaikh204@gmail.com>
2023-07-07 14:49:25 +0530

    3 commit

--- githubOperation/ViewController.swift
+++ githubOperation/ViewController.swift
@@ -9,12 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let flight:String? = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("this is initial commite")
         
-        
+        flight = "airIndia"
         
         print("this is second commit")
     }
